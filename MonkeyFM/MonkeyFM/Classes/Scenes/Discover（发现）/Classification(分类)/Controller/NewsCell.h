443ebfc3e5abdc6c65ad9f51fb0a8da3fc22596f
lanou3g <zhaoce@menglingxu.local>
2016-06-27 09:16:56 +0800

    修改segment

--- MonkeyFM/MonkeyFM/Classes/Scenes/Discover（发现）/Classification(分类)/Controller/NewsCell.h
+++ MonkeyFM/MonkeyFM/Classes/Scenes/Discover（发现）/Classification(分类)/Controller/NewsCell.h
@@ -10,6 +10,28 @@
 
 @interface NewsCell : BaseCell
 
+/// 头像
 @property (nonatomic, strong) UIImageView *pic;
+
+/// 标题
+@property (nonatomic, strong) UILabel *nameLabel;
+
+/// 内容
+@property (nonatomic, strong) UILabel *descLabel;
+
+/// 评论数
+@property (nonatomic, strong) UILabel *hotLabel;
+
+/// 评论数图图片
+@property (nonatomic, strong) UIImageView *hotImageView;
+
+/// 时间
+@property (nonatomic, strong) UILabel *utimeLabel;
+
+/// 时间图片
+@property (nonatomic, strong) UIImageView *utimeImageView;
+
+/// 图片上的button
+@property (nonatomic, strong) UIImageView *button;
 
 @end
--- MonkeyFM/MonkeyFM/Classes/Scenes/Discover（发现）/Classification(分类)/Controller/NewsCell.m
+++ MonkeyFM/MonkeyFM/Classes/Scenes/Discover（发现）/Classification(分类)/Controller/NewsCell.m
@@ -7,7 +7,7 @@
 //
 
 #import "NewsCell.h"
-
+#import <Masonry.h>
 @implementation NewsCell
 
 - (instancetype)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
@@ -20,8 +20,106 @@
 
 - (void)initLayout
 {
-    self.pic = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
+    self.pic = [[UIImageView alloc] init];
     [self.contentView addSubview:self.pic];
+    __weak typeof(self)mySelf = self;
+    [self.pic mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(mySelf.contentView).offset(10);
+        make.left.offset(10);
+        make.bottom.equalTo(mySelf.contentView).offset(-10);
+        make.width.equalTo(mySelf.pic.mas_height);
+    }];
+    // 图片上的button
+    self.button = [[UIImageView alloc] init];
+    self.button.image = [UIImage imageNamed:@"播放button"];
+    [self.pic addSubview:self.button];
+    
+    [self.button mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.width.mas_equalTo(30);
+        make.height.mas_equalTo(30);
+        make.bottom.equalTo(mySelf.pic.mas_bottom).offset(-10);
+        make.right.equalTo(mySelf.pic.mas_right).offset(-10);
+    }];
+    
+    // title
+    self.nameLabel = [[UILabel alloc] init];
+    self.nameLabel.text = @"123456667890-";
+    [self.contentView addSubview:self.nameLabel];
+    [self.nameLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        
+        make.top.equalTo(mySelf.pic.mas_top).offset(10);
+        make.left.equalTo(mySelf.pic.mas_right).offset(10);
+        make.right.equalTo(mySelf.contentView).offset(-10);
+    }];
+    
+    // 内容
+    self.descLabel = [[UILabel alloc] init];
+    self.descLabel.text = @"1234567890-1234567890-1234567890-1234567890-";
+    self.descLabel.textColor = [UIColor lightGrayColor];
+    [self.contentView addSubview:self.descLabel];
+    [self.descLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(mySelf.nameLabel.mas_bottom).offset(20);
+        // 左对齐
+        make.leading.equalTo(mySelf.nameLabel.mas_leading);
+        // 右对齐
+        make.trailing.equalTo(mySelf.nameLabel.mas_trailing);
+        
+    }];
+    
+    // 评论图片
+    self.hotImageView = [[UIImageView alloc] init];
+    self.hotImageView.image  = [UIImage imageNamed:@"评论数"];
+    [self.contentView addSubview:self.hotImageView];
+    [self.hotImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(mySelf.descLabel.mas_bottom).offset(20);
+        make.leading.equalTo(mySelf.descLabel.mas_leading);
+       
+        make.width.mas_equalTo(15);
+        make.height.mas_equalTo(15);
+        
+    }];
+    
+    // 评论数
+    self.hotLabel = [[UILabel alloc] init];
+    [self.contentView addSubview:self.hotLabel];
+    self.hotLabel.text = @"4.3万";
+    self.hotLabel.textColor = [UIColor lightGrayColor];
+    [self.hotLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(mySelf.descLabel.mas_bottom).offset(20);
+        make.left.equalTo(mySelf.hotImageView.mas_right).offset(10);
+        make.width.mas_equalTo(80);
+        
+    }];
+    
+    // 时间
+    self.utimeLabel = [[UILabel alloc] init];
+    self.utimeLabel.text = @"10小时前更新";
+    self.utimeLabel.textColor = [UIColor lightGrayColor];
+    [self.contentView addSubview:self.utimeLabel];
+    [self.utimeLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(mySelf.hotLabel);
+        make.right.equalTo(mySelf.contentView).offset(-10);
+        make.width.mas_equalTo(150);
+        
+    }];
+    
+    // 时间图片
+    self.utimeImageView = [[UIImageView alloc] init];
+    [self.contentView addSubview:self.utimeImageView];
+    self.utimeImageView.image = [UIImage imageNamed:@"时间"];
+    [self.utimeImageView mas_makeConstraints:^(MASConstraintMaker *make) {
+       
+        make.top.equalTo(mySelf.utimeLabel);
+        make.right.equalTo(mySelf.utimeLabel.mas_left).offset(-10);
+        make.width.equalTo(mySelf.hotImageView.mas_width);
+        make.height.equalTo(mySelf.hotImageView.mas_height);
+
+    }];
+    
     
 }
 
